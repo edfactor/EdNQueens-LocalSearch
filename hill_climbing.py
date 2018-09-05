c1002e2baaa98765d0a61f3cc1e9d7d87ba077b4
Ed Lyons <ejlyons@ix.netcom.com>
2018-09-05 14:00:50 -0400

    Add pseudocode to hill climbing

--- hill_climbing.py
+++ hill_climbing.py
@@ -1,5 +1,27 @@
 import random
+""""""
+So what is hill climbing?
 
+Perhaps the pseudo-code will help:
+
+i <- generate an individual randomly
+best_so_far <- i
+while stopping criterion has not been met:
+	get i's bit string and convert it to the problem representation (int or float)
+	increment or decrement one of the genes by the step size
+	if the resulting individual has higher fitness
+		replace i with this individual and increase the step size
+	else
+		decrease the step size
+	if the step size reaches zero and increments and decrements of the current gene have been tested
+		move on to the next gene
+	if i is at a local optimum
+		if fitness(i) > fitness(best_so_far)
+			best_so_far <- i
+		i <- generate an individual randomly
+
+
+""""""
 def hill_climbing(problem):
     # Keep choosing the neighbour with highest value until no neighbor is better
     current = problem.initial()
